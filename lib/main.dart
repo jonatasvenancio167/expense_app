96ded22af1b9b37a05f22e313ac51c29b9a8c33a
jonatas venancio <jvenancio167@hotmail.com>
2024-08-24 18:03:01 -0300

    remove code to create component

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'package:expenses/models/transaction.dart';
+import 'package:expenses/components/transaction_user.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 main() => runApp(ExpenseApp());
 
@@ -14,30 +13,13 @@ class ExpenseApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final titleController = TextEditingController();
-  final valueController = TextEditingController();
-
-  final _transactions = [
-    Transaction(
-      id: 't1',
-      title: 'Salário',
-      value: 5000.00,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Conta de Luz',
-      value: 200.00,
-      date: DateTime.now(),
-    )
-  ];
-
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text('Despesas Pessoais'),
         ),
-        body: Column(
+        body: SingleChildScrollView(
+          child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
               Container(
@@ -47,84 +29,9 @@ class MyHomePage extends StatelessWidget {
                   child: Text('Gráfico'),
                 ),
               ),
-              Column(
-                children: _transactions.map((tr) {
-                  return Card(
-                    child: Row(
-                      children: <Widget>[
-                        Container(
-                          margin: const EdgeInsets.symmetric(
-                            horizontal: 15,
-                            vertical: 10,
-                          ),
-                          decoration: BoxDecoration(
-                            border: Border.all(
-                              color: Colors.purple,
-                              width: 2,
-                            ),
-                          ),
-                          padding: const EdgeInsets.all(10),
-                          child: Text('R\$ ${tr.value.toStringAsFixed(2)}',
-                              style: const TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 20,
-                                color: Colors.purple,
-                              )),
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text(
-                              tr.title,
-                              style: const TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            Text(
-                              DateFormat('d MMM y').format(tr.date),
-                              style: const TextStyle(
-                                color: Colors.grey,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  );
-                }).toList(),
-              ),
-              Card(
-                  elevation: 5,
-                  child: Padding(
-                    padding: const EdgeInsets.all(10),
-                    child: Column(
-                      children: <Widget>[
-                        TextField(
-                          controller: titleController,
-                          decoration: const InputDecoration(
-                            labelText: 'Título',
-                          ),
-                        ),
-                        TextField(
-                          controller: valueController,
-                          decoration: const InputDecoration(
-                            labelText: 'Valor (R\$)',
-                          ),
-                        ),
-                        TextButton(
-                          onPressed: () {
-                            print(titleController.text);
-                            print(valueController.text);
-                          },
-                          style: TextButton.styleFrom(
-                            foregroundColor: Colors.purple,
-                          ),
-                          child: const Text('Adicionar Transação'),
-                        )
-                      ],
-                    ),
-                  ))
-            ]));
+              const TransactionUser(),
+            ],
+          ),
+        ));
   }
 }
