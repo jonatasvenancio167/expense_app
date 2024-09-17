39034bf5576d9a3884d8efa5fbc162019ddc41ae
jonatas venancio <jvenancio167@hotmail.com>
2024-09-17 19:13:25 -0300

    create chart bar to show percentage

---
+++ lib/components/chart.dart
@@ -0,0 +1,51 @@
+import 'package:expenses/components/chart_bar.dart';
+import 'package:expenses/models/transaction.dart';
+import 'package:intl/intl.dart';
+import 'package:flutter/material.dart';
+
+class Chart extends StatelessWidget {
+  final List<Transaction> recentTransactions;
+
+  const Chart(this.recentTransactions, {super.key});
+
+  List<Map<String, Object>> get groupedTransactions {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(
+        Duration(days: index),
+      );
+
+      double totalSum = 0.0;
+
+      for (var i = 0; i < recentTransactions.length; i++) {
+        bool sameDay = recentTransactions[i].date.day == weekDay.day;
+        bool sameMonth = recentTransactions[i].date.month == weekDay.month;
+        bool sameYear = recentTransactions[i].date.year == weekDay.year;
+
+        if (sameDay && sameMonth && sameYear) {
+          totalSum += recentTransactions[i].value;
+        }
+      }
+
+      return {
+        'day': DateFormat.E().format(weekDay)[0],
+        'value': totalSum,
+      };
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        elevation: 6,
+        margin: const EdgeInsets.all(20),
+        child: Row(
+          children: groupedTransactions.map((tr) {
+            return ChartBar(
+              label: tr['day'].toString(),
+              value: tr['value'] as double,
+              percentage: 0.5,
+            );
+          }).toList(),
+        ));
+  }
+}
---
+++ lib/components/chart_bar.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class ChartBar extends StatelessWidget {
+  final String label;
+  final double value;
+  final double percentage;
+
+  ChartBar({
+    required this.label,
+    required this.value,
+    required this.percentage,
+  });
+
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text('r\$ ${value.toStringAsFixed(2)}'),
+        const SizedBox(
+          height: 5,
+        ),
+        Container(
+          height: 60,
+          width: 10,
+          child: Stack(
+            alignment: Alignment.bottomCenter,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.grey,
+                    width: 1.0,
+                  ),
+                  color: const Color.fromRGBO(220, 220, 220, 1),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
+              FractionallySizedBox(
+                heightFactor: percentage,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        SizedBox(
+          height: 5,
+        ),
+        Text(label)
+      ],
+    );
+  }
+}
